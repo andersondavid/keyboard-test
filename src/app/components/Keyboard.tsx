b181aac40ffe77215baad1eff028c33cbea26ed9
Anderson David <andersondavidfreitas@gmail.com>
2023-05-05 22:24:56 -0300

    Add grid css

--- src/app/components/Keyboard.tsx
+++ src/app/components/Keyboard.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import FunctionsKeys from './keyboradLayouts/FunctionsKeys'
-import ModifiersTypewriterKeys from './keyboradLayouts/ModifiersTypewriterKeys'
+import React from "react";
+import FunctionsKeys from "./keyboradLayouts/FunctionsKeys";
+import ModifiersTypewriterKeys from "./keyboradLayouts/ModifiersTypewriterKeys";
+import ArrowsNavigations from "./keyboradLayouts/ArrowsNavigations";
 export default function Keyboard() {
 	return (
-		<div>
-			<FunctionsKeys />
-			<ModifiersTypewriterKeys />
+		<div className="grid grid-flow-col gap-x-2">
+			<div className="col-span-2">
+				<FunctionsKeys />
+			</div>
+			<div className="col-span-2">
+				<ModifiersTypewriterKeys />
+			</div>
+			<div className="row-span-2">
+				<ArrowsNavigations />
+			</div>
 		</div>
-	)
+	);
 }
