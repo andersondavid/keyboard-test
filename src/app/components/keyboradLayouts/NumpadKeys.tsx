6758094b744b501f644238f644b0c730a5eb38ff
Anderson David <andersondavidfreitas@gmail.com>
2023-05-07 22:42:53 -0300

    Add 'code' to NumPad

--- src/app/components/keyboradLayouts/NumpadKeys.tsx
+++ src/app/components/keyboradLayouts/NumpadKeys.tsx
@@ -6,35 +6,36 @@ export default function NumpadKeys() {
 		<div className="flex mt-[5em]">
 			<div className="inline-block">
 				<div>
-					<Key keyValue="NLK" />
-					<Key keyValue="/" />
-					<Key keyValue="*" />
+					<Key keyValue="NLK" code="NumLock" />
+					<Key keyValue="/" code="NumpadDivide" />
+					<Key keyValue="*" code="NumpadMultiply" />
 				</div>
 				<div>
-					<Key keyValue="7" />
-					<Key keyValue="8" />
-					<Key keyValue="9" />
+					<Key keyValue="7" code="Numpad7" />
+					<Key keyValue="8" code="Numpad8" />
+					<Key keyValue="9" code="Numpad9" />
 				</div>
 				<div>
-					<Key keyValue="4" />
-					<Key keyValue="5" />
-					<Key keyValue="6" />
+					<Key keyValue="4" code="Numpad4" />
+					<Key keyValue="5" code="Numpad5" />
+					<Key keyValue="6" code="Numpad6" />
 				</div>
 
 				<div>
-					<Key keyValue="1" />
-					<Key keyValue="2" />
-					<Key keyValue="3" />
+					<Key keyValue="1" code="Numpad1" />
+					<Key keyValue="2" code="Numpad2" />
+					<Key keyValue="3" code="Numpad3" />
 				</div>
 				<div>
-					<Key keyValue="0" />
-					<Key keyValue="." className={"w-[7.5em]"} />
+					<Key keyValue="0" className={"w-[7.5em]"} code="Numpad0" />
+					<Key keyValue="." code='NumpadDecimal'/>
 				</div>
 			</div>
 			<div className="col-start-2 flex flex-col">
-				<Key keyValue="-" />
-				<Key keyValue="+" className={"h-[7.5em]"} />
-				<Key keyValue="↵" className={"h-[7.5em]"} />
+				<Key keyValue="-" code='NumpadSubtract'/>
+				<Key keyValue="+" code='NumpadAdd'/>
+				<Key keyValue="." code='NumpadComma'/>
+				<Key keyValue="↵" className={"h-[7.5em]"} code='NumpadEnter'/>
 			</div>
 		</div>
 	);
